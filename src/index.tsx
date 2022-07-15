6e99794f881b6e8954c734c4e48c657d29c83bdb
mandrade <mandrade@modernatx.com>
2022-07-14 22:41:48 -0300

    big bang

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+
 import App from "./App";
 
 ReactDOM.render(
